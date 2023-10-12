9667fcfa8ffc479cd51bfc89f1d52ce0fb1baaf5
mfarhannugroho <mfarhannugroho08@gmail.com>
2023-10-12 13:11:32 +0700

    first commit

---
+++ database/factories/PemainFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PemainFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nama_pemain' => $this->faker->name,
+            'no_punggung' => $this->faker->randomNumber(2),
+            'posisi' => $this->faker->randomElement(['Penyerang', 'Gelandang', 'Bek', 'Penjaga Gawang']),
+        ];
+    }
+}
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,25 @@
+<footer class="bg-white mt-8 shadow dark:bg-gray-700 ">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
+                
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Farhan.</span>
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-blue-500 sm:mb-0 dark:text-blue-400">
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 text-white">About</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 text-white">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 text-white">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline text-white">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr class="my-6 border-white sm:mx-auto dark:border-white lg:my-8" />
+    </div>
+</footer>
